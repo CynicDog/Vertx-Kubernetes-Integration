1789f8839d42c18de8f00dccbcb94fe98d036d2e
이은상 <kyrsang0801@inbrein.com>
2024-12-04 16:38:48 +0900

    feat: initialize the hazelcast-managed Vert.x cluster with two apps

---
+++ clustered-apps-with-hazelcast-on-k8s/backend/src/main/java/cynicdog/io/Main.java
@@ -0,0 +1,54 @@
+package cynicdog.io;
+
+import io.vertx.core.*;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.spi.cluster.hazelcast.ClusterHealthCheck;
+import io.vertx.ext.healthchecks.HealthCheckHandler;
+import io.vertx.ext.healthchecks.HealthChecks;
+import io.vertx.ext.web.Router;
+
+public class Main extends AbstractVerticle {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    private static final int HTTP_PORT = Integer.parseInt(System.getenv().getOrDefault("HTTP_PORT", "0"));
+    private static final String POD_NAME = System.getenv().getOrDefault("POD_NAME", "unknown");
+
+    @Override
+    public void start() throws Exception {
+
+        registerConsumer();
+
+        Router router = Router.router(vertx);
+        setupRouter(router);
+
+        vertx.createHttpServer()
+                .requestHandler(router)
+                .listen(HTTP_PORT)
+                .onSuccess(server -> logger.info("HTTP server started on port " + server.actualPort()));
+    }
+
+    private void registerConsumer() {
+        vertx.eventBus().<String>consumer("greetings", msg -> {
+            msg.reply(String.format("Hello %s from %s", msg.body(), POD_NAME));
+        });
+    }
+
+    private void setupRouter(Router router) {
+
+        router.get("/health").handler(context -> context.response().end("OK"));
+
+        HealthChecks checks = HealthChecks
+                .create(vertx)
+                .register("cluster-health", ClusterHealthCheck.createProcedure(vertx));
+
+        router.get("/readiness").handler(HealthCheckHandler.createWithHealthChecks(checks));
+    }
+
+    public static void main(String[] args) {
+        Vertx.clusteredVertx(new VertxOptions())
+                .compose(v -> v.deployVerticle(new Main()))
+                .onFailure(Throwable::printStackTrace);
+    }
+}
