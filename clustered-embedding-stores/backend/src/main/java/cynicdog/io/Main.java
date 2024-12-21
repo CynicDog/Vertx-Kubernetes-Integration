634140d232d175acd9c5b91abe6223f08a774d8c
이은상 <kyrsang0801@inbrein.com>
2024-12-21 11:49:52 +0900

    chore:

--- clustered-embedding-stores/backend/src/main/java/cynicdog/io/Main.java
+++ clustered-embedding-stores/backend/src/main/java/cynicdog/io/Main.java
@@ -62,7 +62,7 @@ public class Main extends AbstractVerticle {
         var globalConfig = new GlobalConfigurationBuilder()
                 .transport()
                 .defaultTransport()
-                .addProperty("configurationFile", "default-configs/default-jgroups-kubernetes.xml")
+                // TODO: Jgroup configuration for k8s deployment
                 .build();
 
         // Configure default cache manager
